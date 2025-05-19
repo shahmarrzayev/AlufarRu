a94575ab719b2035a4126221a3a9f6cb778dbfd7
Sadiqovilkin <sadiqovilkin068@gmail.com>
2025-05-19 12:59:05 +0400

    #Home About section finished

--- src/pages/home/about/About.jsx
+++ src/pages/home/about/About.jsx
@@ -1,29 +1,68 @@
-import React from 'react'
-import "./About.scss"
-import HomeAboutSvg from '../../../assets/svg/HomeAboutSvg'
+import React, { useEffect } from "react";
+import "./About.scss";
+import { motion, useAnimation, useInView } from "framer-motion";
+import HomeAboutSvg from "../../../assets/svg/HomeAboutSvg";
+import { useRef } from "react";
+
 const About = () => {
+  const controlsSvg = useAnimation();
+  const controlsTxt = useAnimation();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.5 });
+
+  useEffect(() => {
+    if (isInView) {
+      controlsSvg.start({ width: 0 });
+      controlsTxt.start({ height: 0 });
+    } else {
+      controlsSvg.set({ width: "100%" });
+      controlsTxt.set({ height: "100%" });
+    }
+  }, [isInView, controlsSvg, controlsTxt]);
+
   return (
-    <section id='homeAbout'>
+    <section id="homeAbout" ref={ref}>
       <div className="container">
         <div className="row align-items-center">
           <div className="col-lg-7">
             <div className="aboutContent">
               <h1>О НАС</h1>
               <div className="border"></div>
-              <p>Компания «Alufar» была создана в Азербайджане в 2018 году</p>
-              <p>Мы специализируемся на производстве и продаже алюминиевых промышленных и мебельных профилей, мебельной фурнитуры и алюминиевых фасадных систем.</p>
-              <p>Наша компания — это команда профессионалов, работающих над тем, чтобы сделать процесс строительства проще и удобнее.</p>
+              <div className="texts">
+                <motion.div
+                  className="aboutLoadingTxt"
+                  initial={{ height: "100%" }}
+                  animate={controlsTxt}
+                  transition={{ duration: 1.5, ease: "easeInOut" }}
+                />
+                <p>Компания «Alufar» была создана в Азербайджане в 2018 году</p>
+                <p>
+                  Мы специализируемся на производстве и продаже алюминиевых
+                  промышленных и мебельных профилей, мебельной фурнитуры и
+                  алюминиевых фасадных систем.
+                </p>
+                <p>
+                  Наша компания — это команда профессионалов, работающих над
+                  тем, чтобы сделать процесс строительства проще и удобнее.
+                </p>
+              </div>
             </div>
           </div>
           <div className="col-lg-5">
             <div className="aboutImage">
-              <HomeAboutSvg/>
+              <motion.div
+                className="aboutLoadingImage"
+                initial={{ width: "100%" }}
+                animate={controlsSvg}
+                transition={{ duration: 1 }}
+              />
+              <HomeAboutSvg />
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
